dcc0f71dc1b1efab82d75daaa21309237f0e26e2
JonesApoorva8 <saiapoorvapaila@gmail.com>
2021-05-25 20:57:52 +0530

    changed the content in hi file

--- hi.java
+++ hi.java
@@ -1,14 +1,6 @@
 
-public class Pattern {
-public static void main(String[] args) {
-	int number = 1;
-	for(int i=1;i<=10;i++) 
-	{
-		for(int j=1;j<=i;j++)
-		{
-			System.out.print(number+ " ");
-			number++;
-		}
-	}
-	}
+public class hi{
+publis static void main(String[] args){
+System.out.println("Hi ...  This is a sampe java program")
+}
 }
